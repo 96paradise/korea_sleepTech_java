392ed0b5ed9f62d3d2fd34cffb30aea2be809563
96paradise <cnw0806@naver.com>
2025-04-05 17:01:09 +0900

    20250405 CNW

--- src/test/practice/star.java
+++ src/test/practice/star.java
@@ -1,6 +1,13 @@
 package test.practice;
+/*
+			*
+	        **
+	        ***
+	        ****
+	        *****
+ */
 
-public class star {
+public class Star {
 	public static void main(String[] args) {
 		for(int i = 1; i <= 5; i++) {
 			for(int j = 1; j <= i; j++) {
