7ec003c97b6fb95a29884e326df80af9aab13fbd
96paradise <cnw0806@naver.com>
2025-04-12 20:45:54 +0900

    20250412 CNW

---
+++ src/test/practice/chapter04/인스턴스_클래스_변수.java
@@ -0,0 +1,26 @@
+package test.practice.chapter04;
+
+class Card {
+	// 인스턴스 변수
+	int number;
+	String kind;
+	
+	// 클래스 변수
+	static int width = 100;
+	static int height = 200;
+}
+
+public class 인스턴스_클래스_변수 {
+	public static void main(String[] args) {
+		System.out.println(Card.width);
+		System.out.println(Card.height);
+		
+//		System.out.println(Card.number);
+		
+		Card c1 = new Card();
+		c1.kind = "Spade";
+		c1.number = 7;
+		
+		System.out.println(c1.kind + c1.number);
+	}
+}
